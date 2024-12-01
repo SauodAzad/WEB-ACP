add5bed4e4beb85e5392d4b93b105765ea3f3633
Sauod Azad <sauodazd@gmail.com>
2024-12-01 19:13:31 +0500

    Delete FurnitureManagementSystem2 directory

--- FurnitureManagementSystem2/src/furnituremanagementsystem2/FurnitureManagementSystem2.java
+++
@@ -1,9 +0,0 @@
-package furnituremanagementsystem2;
-
-import javax.swing.SwingUtilities;
-
-public class FurnitureManagementSystem2 {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new SelectionScreen().createAndShowSelectionScreen());
-    }
-}
